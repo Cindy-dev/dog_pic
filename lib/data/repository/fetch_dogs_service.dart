13973e96ab224ea5f17dbd3f0426ff017158c6a3
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2026-02-15 19:30:12 +0100

    api error update

--- lib/data/repository/fetch_dogs_service.dart
+++ lib/data/repository/fetch_dogs_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dog_pic/common/utils/api_error.dart';
 import 'package:dog_pic/data/models/dogs_model.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -10,7 +11,10 @@ class FetchDogsService {
       final res = await dio.get("https://dog.ceo/api/breeds/image/random");
       final responseModel = DogsModel.fromJson(res.data);
       return responseModel;
-    } catch (e) {
+    } on DioException catch (e) {
+      final apiError = ApiError.fromDioError(e);
+      return Future.error(apiError);
+    } catch (_) {
       rethrow;
     }
   }
