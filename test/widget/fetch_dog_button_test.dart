d0aaf7629bc804efc063fc4a0a053a6e5f74044b
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2026-02-16 21:44:15 +0100

    widget test implementation

--- test/widget/fetch_dog_button_test.dart
+++ test/widget/fetch_dog_button_test.dart
@@ -1,13 +1,19 @@
 import 'package:dog_pic/data/models/dogs_model.dart';
-import 'package:dog_pic/data/repository/fetch_dogs_service.dart';
+import 'package:dog_pic/logic/dog_fetch_state.dart';
+import 'package:dog_pic/logic/fetch_dog_view_model.dart';
 import 'package:dog_pic/presentation/screens/dog_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class FakeDogsService extends FetchDogsService {
+class FakeDogsViewModel extends FetchDogViewModel {
+  FakeDogsViewModel(super.ref);
   @override
-  Future<DogsModel> fetchRandomDogImages() async {
+  Future<DogsModel> fetchDogs() async {
+    state = SuccessState(
+      dogsModel: DogsModel(message: "", status: "success"),
+    );
     return DogsModel(message: "", status: "success");
   }
 }
@@ -16,12 +22,36 @@ void main() {
   testWidgets('Press button calls fetchDogs()', (tester) async {
     await tester.pumpWidget(
       ProviderScope(
-        overrides: [dogsServiceProvider.overrideWithValue(FakeDogsService())],
+        overrides: [
+          fetchDogViewModel.overrideWith((ref) => FakeDogsViewModel(ref)),
+        ],
         child: const MaterialApp(home: DogView()),
       ),
     );
     expect(find.text('Press'), findsOneWidget);
     await tester.tap(find.text('Press'));
-    await tester.pumpAndSettle();
+    await tester.pump();
+    expect(find.byType(CupertinoActivityIndicator), findsOneWidget);
   });
 }
+
+// class FakeDogsService extends FetchDogsService {
+//   @override
+//   Future<DogsModel> fetchRandomDogImages() async {
+//     return DogsModel(message: "", status: "success");
+//   }
+// }
+//
+// void main() {
+//   testWidgets('Press button calls fetchDogs()', (tester) async {
+//     await tester.pumpWidget(
+//       ProviderScope(
+//         overrides: [dogsServiceProvider.overrideWithValue(FakeDogsService())],
+//         child: const MaterialApp(home: DogView()),
+//       ),
+//     );
+//     expect(find.text('Press'), findsOneWidget);
+//     await tester.tap(find.text('Press'));
+//     await tester.pumpAndSettle();
+//   });
+// }
